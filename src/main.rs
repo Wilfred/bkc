b6dc02d425708addaddecccc1b9c3301515e3e93
Wilfred Hughes <me@wilfred.me.uk>
2016-10-08 00:04:16 -0400

    Read an ELF file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate elf;
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let path = PathBuf::from("/home/wilfred/projects/tiddles/exit.o");
+    let file = match elf::File::open_path(&path) {
+        Ok(f) => f,
+        Err(e) => panic!("Error: {:?}", e),
+    };
+
+    let text_scn = match file.get_section(".text") {
+        Some(s) => s,
+        None => panic!("Failed to look up .text section"),
+    };
+
+    println!("{:?}", text_scn.data);
 }
