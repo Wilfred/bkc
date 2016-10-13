002ad16821b069074ef2e1cb3497ea366b7caf7d
Wilfred Hughes <me@wilfred.me.uk>
2016-10-12 23:09:56 -0400

    Write full header from elf::File

--- src/main.rs
+++ src/main.rs
@@ -5,90 +5,119 @@ use std::path::PathBuf;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Error;
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{LittleEndian, BigEndian, WriteBytesExt};
 
 use elf::types::*;
 
-fn write_elf_file(file: elf::File, filename: &str) -> Result<(), Error> {
+// TODO: Make a write_u16! macro to be consistent with the elf
+// library.
+fn write_u16(mut out: &File, value: u16, elf_header: FileHeader) -> Result<(), Error> {
+    match elf_header.data {
+        ELFDATA2LSB => out.write_u16::<LittleEndian>(value),
+        ELFDATA2MSB => out.write_u16::<BigEndian>(value),
+        ELFDATANONE => {
+            panic!("Unable to resolve file endianness");
+        }
+        _ => {
+            panic!("Unable to resolve file endianness");
+        }
+    }
+}
+
+fn write_u32(mut out: &File, value: u32, elf_header: FileHeader) -> Result<(), Error> {
+    match elf_header.data {
+        ELFDATA2LSB => out.write_u32::<LittleEndian>(value),
+        ELFDATA2MSB => out.write_u32::<BigEndian>(value),
+        ELFDATANONE => {
+            panic!("Unable to resolve file endianness");
+        }
+        _ => {
+            panic!("Unable to resolve file endianness");
+        }
+    }
+}
+
+fn write_u64(mut out: &File, value: u64, elf_header: FileHeader) -> Result<(), Error> {
+    match elf_header.data {
+        ELFDATA2LSB => out.write_u64::<LittleEndian>(value),
+        ELFDATA2MSB => out.write_u64::<BigEndian>(value),
+        ELFDATANONE => {
+            panic!("Unable to resolve file endianness");
+        }
+        _ => {
+            panic!("Unable to resolve file endianness");
+        }
+    }
+}
+
+// Based on https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+// and reading the excellent Rust elf library.
+fn write_elf_file(elf_file: elf::File, filename: &str) -> Result<(), Error> {
     let mut buffer = try!(File::create(filename));
 
     // Write the ELF magic number.
     try!(buffer.write(&[ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3]));
 
-    try!(buffer.write(&[file.ehdr.class.0]));
-    try!(buffer.write(&[file.ehdr.data.0]));
+    try!(buffer.write(&[elf_file.ehdr.class.0]));
+    try!(buffer.write(&[elf_file.ehdr.data.0]));
 
     // TODO: add this to the elf::File struct.
     try!(buffer.write(&[1]));
 
-    try!(buffer.write(&[file.ehdr.osabi.0]));
-    try!(buffer.write(&[file.ehdr.abiversion]));
+    try!(buffer.write(&[elf_file.ehdr.osabi.0]));
+    try!(buffer.write(&[elf_file.ehdr.abiversion]));
 
     // Currently unused in ELF.
     try!(buffer.write(&[0; 7]));
 
+    try!(write_u16(&mut buffer, elf_file.ehdr.elftype.0, elf_file.ehdr));
+    try!(write_u16(&mut buffer, elf_file.ehdr.machine.0, elf_file.ehdr));
+    try!(write_u32(&mut buffer, elf_file.ehdr.version.0, elf_file.ehdr));
+
+    if elf_file.ehdr.class == ELFCLASS32 {
+        unimplemented!();
+    } else {
+        try!(write_u64(&mut buffer, elf_file.ehdr.entry, elf_file.ehdr));
+
+        // TODO: phoff isn't in the File struct, fix that.
+        let phoff = 0;
+        try!(write_u64(&mut buffer, phoff, elf_file.ehdr));
+
+        // TODO: calculate this value somehow.
+        let shoff = 264;
+        try!(write_u64(&mut buffer, shoff, elf_file.ehdr));
+
+        // TODO: these values aren't in the file header struct either.
+        let flags = 0;
+        try!(write_u32(&mut buffer, flags, elf_file.ehdr));
+    }
+
+    // Header size.
+    if elf_file.ehdr.class == ELFCLASS32 {
+        try!(write_u16(&mut buffer, 52, elf_file.ehdr));
+    } else {
+        try!(write_u16(&mut buffer, 64, elf_file.ehdr));
+    }
+
+    // TODO: add to struct?
+    let phentsize = 0;
+    try!(write_u16(&mut buffer, phentsize, elf_file.ehdr));
+
+    let phnum = elf_file.phdrs.len() as u16;
+    try!(write_u16(&mut buffer, phnum, elf_file.ehdr));
+
+    // TODO: calculate this.
+    let shentsize = 64;
+    try!(write_u16(&mut buffer, shentsize, elf_file.ehdr));
+
+    let shnum = elf_file.sections.len() as u16;
+    try!(write_u16(&mut buffer, shnum, elf_file.ehdr));
+
+    // TODO: iterate through sections to find this value.
+    let shstrndx = 4;
+    try!(write_u16(&mut buffer, shstrndx, elf_file.ehdr));
+
     println!("wrote copy {}", filename);
-    Ok(())
-}
-
-// Based on https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
-// and reading the excellent Rust elf library.
-fn write_elf64(name: &str) -> Result<(), Error> {
-    let mut buffer = try!(File::create(name));
-    // Write the ELF magic number.
-    try!(buffer.write(&[ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3]));
-
-    // We want a 64 bit ELF file.
-    try!(buffer.write(&[ELFCLASS64.0]));
-
-    // Little-endian.
-    try!(buffer.write(&[ELFDATA2LSB.0]));
-
-    // Zero-pad to 16 bytes, so 'System V' and default ABI.
-    try!(buffer.write(&[0u8; 10]));
-
-    // Relocatable.
-    try!(buffer.write_u16::<LittleEndian>(ET_REL.0));
-
-    // x86-64 architecture.
-    try!(buffer.write_u16::<LittleEndian>(EM_X86_64.0));
-
-    // ELF v1
-    try!(buffer.write_u32::<LittleEndian>(EV_CURRENT.0));
-
-    // Entry point for executing the process.
-    try!(buffer.write_u64::<LittleEndian>(0));
-
-    // Program header immediately follows elf header, so offset 0.
-    try!(buffer.write_u64::<LittleEndian>(0));
-
-    // TODO: calculate the correct section header offset.
-    // This value is just blindly copy-pasted from exit.o.
-    try!(buffer.write_u64::<LittleEndian>(264));
-
-    // Flags can be zero on x86-64.
-    try!(buffer.write_u32::<LittleEndian>(0));
-
-    // Header size, which is 64 bytes.
-    try!(buffer.write_u16::<LittleEndian>(64));
-
-    // Program header table entry size.
-    try!(buffer.write_u16::<LittleEndian>(0));
-
-    // Number of entries in program header entry table.
-    try!(buffer.write_u16::<LittleEndian>(0));
-
-    // Size of a section header table entry.
-    try!(buffer.write_u16::<LittleEndian>(64));
-
-    // The number of sections we will write (which is the number of
-    // entries in the section header table).
-    try!(buffer.write_u16::<LittleEndian>(7));
-
-    // The index in the section header table of section names.
-    try!(buffer.write_u16::<LittleEndian>(4));
-
-    println!("wrote {}", name);
     Ok(())
 }
 
@@ -99,6 +128,5 @@ fn main() {
         Err(e) => panic!("Error: {:?}", e),
     };
 
-    write_elf64("tiddles_out.o").unwrap();
     write_elf_file(file, "copy.o").unwrap();
 }
